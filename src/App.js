e4bb3c52f64e446aadc2fa46a47ff4884871e1f3
flazed <58736763+flazed@users.noreply.github.com>
2021-09-07 21:43:32 +0300

    fix fullscreen

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './main.scss';
+import VideoPlayer from './VideoPlayer/videoplayer';
+
+export default function App() {
+  return (
+    <VideoPlayer url="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"/>
+    )
+}
---
+++ src/VideoPlayer/icons/index.js
@@ -0,0 +1,18 @@
+// Volume
+export {default as volume_0} from './volume/volume-0.svg'
+export {default as volume_50} from './volume/volume-50.svg'
+export {default as volume_100} from './volume/volume-100.svg'
+
+// Screen-control
+export {default as pip} from './screen-controls/pip.svg'
+export {default as to_fullscreen} from './screen-controls/to-fullscreen.svg'
+export {default as out_fullscreen} from './screen-controls/out-fullscreen.svg'
+
+// Player-controls
+export {default as loop} from './player-controls/loop.svg'
+export {default as rewind_back} from './player-controls/rewind-back.svg'
+export {default as play} from './player-controls/play.svg'
+export {default as pause} from './player-controls/pause.svg'
+export {default as rewind_forward} from './player-controls/rewind-forward.svg'
+export {default as favorite} from './player-controls/favorite.svg'
+export {default as main_play} from './player-controls/main-play.svg'
